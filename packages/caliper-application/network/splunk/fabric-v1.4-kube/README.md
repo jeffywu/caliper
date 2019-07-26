a143bf50fc7a0a703b22e9be0093e5a98a17f993
Jeff Wu <jeffreywu@splunk.com>
2019-07-26 09:54:45 -0700

    Some changes to ADI contract and config.

--- packages/caliper-application/network/splunk/fabric-v1.4-kube/README.md
+++ packages/caliper-application/network/splunk/fabric-v1.4-kube/README.md
@@ -1,3 +1,16 @@
+# Prerequisites
+For AWS, make sure that you have `kubectl` and `aws-iam-authenticator` installed. See these docs:
+  * https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html
+  * https://docs.aws.amazon.com/eks/latest/userguide/install-aws-iam-authenticator.html
+
+Then follow these instructions to setup your kubeconfig:
+```
+aws --version     # Check that this is > 1.16.156
+aws eks --region region update-kubeconfig --name cluster_name
+kubectl get svc
+```
+More details at: https://docs.aws.amazon.com/eks/latest/userguide/create-kubeconfig.html
+
 # Running a Remote Test on Kubernetes
 This network config enables you to run a remote test on a kubernetes environment. Setting up the test is simple.
 
--- packages/caliper-application/src/contract/fabric/adi/adi.go
+++ packages/caliper-application/src/contract/fabric/adi/adi.go
@@ -1,5 +1,5 @@
 /**
-* Copyright 2017 HUAWEI. All Rights Reserved.
+* Copyright 2019 Splunk. All Rights Reserved.
 *
 * SPDX-License-Identifier: Apache-2.0
 *
@@ -16,14 +16,15 @@ import (
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
-const ERROR_SYSTEM = "{\"code\":300, \"reason\": \"system error: %s\"}"
-const ERROR_WRONG_FORMAT = "{\"code\":301, \"reason\": \"command format is wrong\"}"
-const COMPOSITE_KEY_FORMAT = "sender~bucket~index"
+const errorSystem = "{\"code\":300, \"reason\": \"system error: %s\"}"
+const errorWrongFormat = "{\"code\":301, \"reason\": \"command format is wrong\"}"
+const compositeKeyFormat = "sender~bucket~index"
 
+// DataIntegrityChaincode is an interface for the chaincode
 type DataIntegrityChaincode struct {
 }
 
-// Return type for a query action.
+// Response is the return type for a query action.
 type Response struct {
 	Sender string `json:"sender"`
 	Bucket string `json:"bucket"`
@@ -31,11 +32,13 @@ type Response struct {
 	Hash   string `json:"hash"`
 }
 
+// Init chaincode (nothing to do)
 func (t *DataIntegrityChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	// nothing to do
 	return shim.Success(nil)
 }
 
+// Invoke chaincode with submitHash or queryHash
 func (t *DataIntegrityChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	function, args := stub.GetFunctionAndParameters()
 
@@ -46,30 +49,30 @@ func (t *DataIntegrityChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Res
 		return t.queryHash(stub, args)
 	}
 
-	return shim.Error(ERROR_WRONG_FORMAT)
+	return shim.Error(errorWrongFormat)
 }
 
 // Write a new hash value [submitHash bucket index hash]
 func (t *DataIntegrityChaincode) submitHash(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) != 3 {
-		return shim.Error(ERROR_WRONG_FORMAT)
+		return shim.Error(errorWrongFormat)
 	}
 	senderBytes, err := stub.GetCreator()
 	if err != nil {
-		s := fmt.Sprintf(ERROR_SYSTEM, err.Error())
+		s := fmt.Sprintf(errorSystem, err.Error())
 		return shim.Error(s)
 	}
 	senderHex := hex.EncodeToString(senderBytes)
 
-	key, err := stub.CreateCompositeKey(COMPOSITE_KEY_FORMAT, []string{senderHex, args[0], args[1]})
+	key, err := stub.CreateCompositeKey(compositeKeyFormat, []string{senderHex, args[0], args[1]})
 	if err != nil {
-		s := fmt.Sprintf(ERROR_SYSTEM, err.Error())
+		s := fmt.Sprintf(errorSystem, err.Error())
 		return shim.Error(s)
 	}
 
 	err = stub.PutState(key, []byte(args[2]))
 	if err != nil {
-		s := fmt.Sprintf(ERROR_SYSTEM, err.Error())
+		s := fmt.Sprintf(errorSystem, err.Error())
 		return shim.Error(s)
 	}
 
@@ -79,11 +82,11 @@ func (t *DataIntegrityChaincode) submitHash(stub shim.ChaincodeStubInterface, ar
 // Query for values of sender [queryHash sender]
 func (t *DataIntegrityChaincode) queryHash(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) != 1 {
-		return shim.Error(ERROR_WRONG_FORMAT)
+		return shim.Error(errorWrongFormat)
 	}
 	sender := args[0]
 
-	resultsIterator, err := stub.GetStateByPartialCompositeKey(COMPOSITE_KEY_FORMAT, []string{sender})
+	resultsIterator, err := stub.GetStateByPartialCompositeKey(compositeKeyFormat, []string{sender})
 	if err != nil {
 		return shim.Error(fmt.Sprintf("Could not retrieve value for %s: %s", sender, err.Error()))
 	}
